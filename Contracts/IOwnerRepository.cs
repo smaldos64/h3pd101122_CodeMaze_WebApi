23156dd38fce79909001435e29d45ed62dee6eff
Lars Thise Pedersen <ltpe@techcollege.dk>
2022-11-28 08:53:00 +0100

    Add project files.

---
+++ Contracts/IOwnerRepository.cs
@@ -0,0 +1,14 @@
+﻿using Entities.Models;
+
+namespace Contracts
+{
+    public interface IOwnerRepository : IRepositoryBase<Owner>
+    {
+        IEnumerable<Owner> GetAllOwners(bool IncludeRelations);
+        Owner GetOwnerById(Guid ownerId);
+        Owner GetOwnerWithDetails(Guid ownerId);
+        void CreateOwner(Owner owner);
+        void UpdateOwner(Owner owner);
+        void DeleteOwner(Owner owner);
+    }
+}
